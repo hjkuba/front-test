3835a02aa9a0a0f699ce34ee63cac596222608e5
Heitor Kuba <hjkuba@yahoo.com.br>
2017-12-13 23:55:37 -0200

    Implementing Pricing Body styles

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,9 @@
 import SexLogModal from './SexLogModal';
 import SexLogHeader from './SexLogHeader';
+import SexLogBody from './SexLogBody';
+import PriceContainer from './PriceContainer';
+import PlanOptionsContainer from './PlanOptionsContainer';
+import PlanOption from './PlanOption';
 
-export { SexLogModal, SexLogHeader };
+export { SexLogModal, SexLogHeader, SexLogBody, PriceContainer,
+    PlanOptionsContainer, PlanOption };
